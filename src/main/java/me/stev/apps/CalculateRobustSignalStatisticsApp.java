2e4817910d1fe5b9211f18c891f637681010a983
Fredericia <39918560+stevD9@users.noreply.github.com>
2019-12-14 15:52:00 +0200

    refactor

--- src/main/java/me/stev/apps/CalculateRobustSignalStatisticsApp.java
+++ src/main/java/me/stev/apps/CalculateRobustSignalStatisticsApp.java
@@ -1,14 +1,13 @@
 package me.stev.apps;
 
 import me.stev.algorithms.robust.RobustSignalStatistics;
-import me.stev.algorithms.simple.SignalStatistics;
 import me.stev.signals.Signal;
 
 public class CalculateRobustSignalStatisticsApp {
 
     public static void main(String[] args) {
 
-        double mean = SignalStatistics.calcSigMean(Signal.InputSignal_f32_1kHz_15kHz, Signal.InputSignal_f32_1kHz_15kHz.length);
+        double mean = RobustSignalStatistics.calcSigMean(Signal.InputSignal_f32_1kHz_15kHz, Signal.InputSignal_f32_1kHz_15kHz.length);
         System.out.println("The mean is : " + mean);
     }
 }
